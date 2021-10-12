479642afb36de7f9b2af748482aabe9fc8f1821b
Diego Alloza González <sralloza@gmail.com>
2021-10-12 19:37:38 +0200

    chore: import only cronjobs in app.cron

--- app/cron/__init__.py
+++ app/cron/__init__.py
@@ -1,4 +1,4 @@
-from .backup_database import *
+from .backup_database import backup_database
 from .base import scheduler
-from .check_frozen_meals import *
-from .update_notion_meals import *
+from .check_frozen_meals import check_frozen_meals
+from .update_notion_meals import update_notion_meals
