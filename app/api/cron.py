3ae76cb536127158119fa8e4a79cf6390783e68d
Diego Alloza González <sralloza@gmail.com>
2021-10-16 10:39:26 +0200

    feat: add endpoints to launch cron scripts

---
+++ app/api/cron.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+from fastapi import APIRouter, Depends, BackgroundTasks
+
+from ..deps.security import token_middleware
+from ..utils.responses import gen_responses
+from ..cron import backup_database, check_frozen_meals, update_notion_meals
+
+router = APIRouter(
+    tags=["Cron"],
+    dependencies=[Depends(token_middleware)],
+    **gen_responses({401: "Missing Token", 403: "Invalid token"}),
+)
+
+
+class ValidCron(Enum):
+    backup_database = "backup-database"
+    check_frozen_meals = "check-frozen-meals"
+    update_notion_meals = "update-notion-meals"
+
+
+CRON_MAP = {
+    ValidCron.backup_database: backup_database,
+    ValidCron.check_frozen_meals: check_frozen_meals,
+    ValidCron.update_notion_meals: update_notion_meals,
+}
+
+
+@router.post(
+    "/{cron}",
+    response_model_exclude_unset=True,
+    response_model=ValidCron,
+    status_code=201,
+    summary="Launch Cron",
+)
+def get_meals(cron: ValidCron, background_tasks: BackgroundTasks):
+    """Launches a cronjob in the background."""
+    background_tasks.add_task(CRON_MAP[cron])
+    return cron
