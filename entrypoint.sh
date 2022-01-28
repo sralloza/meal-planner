c699b35e54b208f9282831d12e9e868cfadf3398
Diego Alloza González <diego.allozagonzalez.practicas@telefonica.com>
2022-01-28 09:26:00 +0100

    chore: raise timeout to 120 seconds

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 if [[ -z "${WAIT_FOR_IT_ADDRESS}" ]]; then
   echo "Skipping wait-for-it ($$WAIT_FOR_IT_ADDRESS is not defined)"
 else
-  echo "running wait-for-it.sh -t 30 ${WAIT_FOR_IT_ADDRESS}"
-  /wait-for-it.sh -t 30 "${WAIT_FOR_IT_ADDRESS}"
+  echo "running wait-for-it.sh -t 120 ${WAIT_FOR_IT_ADDRESS}"
+  /wait-for-it.sh -t 120 "${WAIT_FOR_IT_ADDRESS}"
 fi
 
 python ./scripts/ensure-database.py
