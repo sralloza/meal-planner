ab95896ab947e2c308b37aa0312212c0fb878ca2
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-05-10 15:29:38 +0200

    chore: pylint suggestions

--- app/crud/base.py
+++ app/crud/base.py
@@ -9,15 +9,15 @@ from sqlalchemy.orm import Session
 
 from ..db.base_class import Base
 
-ModelType = TypeVar("ModelType", bound=Base)
-CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
-UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+Model = TypeVar("Model", bound=Base)
+CreateSchema = TypeVar("CreateSchema", bound=BaseModel)
+UpdateSchema = TypeVar("UpdateSchema", bound=BaseModel)
 
 _Id = date
 # pylint: disable=redefined-builtin
 
 
-class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+class CRUDBase(Generic[Model, CreateSchema, UpdateSchema]):
     """Base CRUD class."""
 
     def raise_not_found_error(self, *, id: _Id):
@@ -30,7 +30,7 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         detail = f"{self.model.__name__} with id={id} already exists"
         raise HTTPException(409, detail)
 
-    def __init__(self, model: Type[ModelType]):
+    def __init__(self, model: Type[Model]):
         """
         CRUD object with default methods to Create, Read, Update, Delete (CRUD).
         **Parameters**
@@ -39,11 +39,11 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         """
         self.model = model
 
-    def get(self, db: Session, id: _Id) -> Optional[ModelType]:
+    def get(self, db: Session, id: _Id) -> Optional[Model]:
         """Get an object using its id."""
         return db.query(self.model).filter(self.model.id == id).first()
 
-    def get_or_404(self, db: Session, id: _Id) -> ModelType:
+    def get_or_404(self, db: Session, id: _Id) -> Model:
         """Get an object or return 404."""
         obj = self.get(db, id=id)
         if obj is not None:
@@ -52,14 +52,14 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
 
     def get_multi(
         self, db: Session, *, skip: int = 0, limit: int = 100
-    ) -> List[ModelType]:
+    ) -> List[Model]:
         """Get multiple objects."""
 
         return db.query(self.model).offset(skip).limit(limit).all()
 
     def create(
-        self, db: Session, *, obj_in: CreateSchemaType, commit_refresh=True
-    ) -> ModelType:
+        self, db: Session, *, obj_in: CreateSchema, commit_refresh=True
+    ) -> Model:
         """Create an object."""
 
         obj_in_data = obj_in.dict()
@@ -77,9 +77,9 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
     def update(
         db: Session,
         *,
-        db_obj: ModelType,
-        obj_in: UpdateSchemaType,
-    ) -> ModelType:
+        db_obj: Model,
+        obj_in: UpdateSchema,
+    ) -> Model:
         """Update an object."""
 
         obj_data = obj_in.dict(exclude_unset=True)
