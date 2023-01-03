d7937a9a9fc565ece2bd545a13d6f35c1a7aab47
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2023-01-03 17:58:56 +0100

    delivery: fix dockerfile

--- delivery/entrypoint.sh
+++ delivery/entrypoint.sh
@@ -1,18 +1,18 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 set -e
 
 if [[ -z "${WAIT_FOR_IT_ADDRESS}" ]]; then
   echo "Skipping wait-for-it ($$WAIT_FOR_IT_ADDRESS is not defined)"
 else
   echo "running wait-for-it.sh -t 120 ${WAIT_FOR_IT_ADDRESS}"
-  /wait-for-it.sh -t 120 "${WAIT_FOR_IT_ADDRESS}"
+  /delivery/wait-for-it.sh -t 120 "${WAIT_FOR_IT_ADDRESS}"
 fi
 
 echo "Ensuring database existence"
-python ./scripts/ensure-database.py
+python /scripts/ensure-database.py
 
 echo "Upgrading database"
 alembic upgrade head
 
 echo "Launching uvicorn server"
-exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT}
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
